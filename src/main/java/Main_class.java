81a4746d3c06a053bffecb1f3df9023cb088d3a0
Silvio <molinahuel44@gmail.com>
2021-04-02 23:24:25 -0300

    first commit

---
+++ src/main/java/Main_class.java
@@ -0,0 +1,12 @@
+package Main_class;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+public class Main_class {
+    public static void main(String[] args){
+        new HttpService {
+            url ""
+        }
+    }
+}
