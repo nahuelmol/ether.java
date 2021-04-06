cc272868e6f5a149bb917ba210d201c39a57179f
Silvio Molina <molinahuel44@gmail.com>
2021-04-06 20:34:09 -0300

    attempting to run, i should learn more about goals and phases in maven, it's difficult to understand for me

--- src/main/java/com/mycompany/mavenproject/ethereum/MainClass.java
+++ src/main/java/com/mycompany/mavenproject/ethereum/MainClass.java
@@ -5,10 +5,19 @@
  */
 package com.mycompany.mavenproject.ethereum;
 
-/**
- *
- * @author Alumno
- */
+
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+
+import java.lang.*;
+import java.util.concurrent.ExecutionException;
+
 public class MainClass {
-    
+    public static void main(String[] args)throws ExecutionException, InterruptedException {
+        Web3j web3 = Web3j.build(new HttpService("https://mainnet.infura.io/v3/008192f0ecd445f2bd16825205da6f89"));
+        EthBlockNumber result = web3.ethBlockNumber().sendAsync().get();
+        System.out.println("The block number is: "+ result.getBlockNumber().toString());
+                
+    }
 }
